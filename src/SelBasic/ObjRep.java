ae5c0cd49e3c4b39b2d60be2d779470b2a69bf71
Parag Bangad <paragbangad@hotmail.com>
2017-09-10 00:50:56 +0530

    first commit

---
+++ src/SelBasic/ObjRep.java
@@ -0,0 +1,16 @@
+package SelBasic;
+
+public class ObjRep {
+	String textInItem1 = "Item1";
+
+	String tagValue = "value";
+
+	String TEXTBOX1 = "ContentPlaceHolder1_TextBox1";
+
+	String URLListBox = "http://automationsite.azurewebsites.net/ListBox.aspx";
+
+	String ITEM1 = textInItem1;
+	String checkBoxID ="ctl00_ContentPlaceholder1_RadCheckBox1";
+	
+	public final String listBox1 = "ContentPlaceHolder1_ListBox1";
+}
